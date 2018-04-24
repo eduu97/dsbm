d68645f289b0a53824388428572cf18348446df4
eduu97 <36338699+eduu97@users.noreply.github.com>
2018-04-24 19:11:12 +0200

    Update accel.c

--- p3/accel.c
+++ p3/accel.c
@@ -4,51 +4,154 @@
 
  Accelerometer module source file
 
-***************************************************************/
+ ***************************************************************/
 
 #include "Base.h"    // Basic definitions
 #include "accel.h"   // Header file for this module
-
 // Some functions need to be implemented as indicated on P3
 
 /********** PUBLIC FUNCTIONS TO IMPLEMENT ************************
  Those functions will be used outside of lcd.c so there will
  be prototypes for them in lcd.h.
-******************************************************************/
+ ******************************************************************/
 
 // Init the accelerometer SPI bus
-void initAccel(void)
- {
 
-	RCC->APB2ENR = (RCC->APB2ENR)|(RCC_APB2ENR_SPI1EN);		//2^12 bit SPI1 EN (constant defined)
-
-	SPI1->CR1 = (SPI1->CR1)|SPI_CR1_CPOL;		//Inactive line for high level
-	SPI1->CR1 = (SPI1->CR1)|SPI_CR1_CPHA;		//Data is read with the second flank of the clock
-
-	SPI1->CR1 = (SPI1->CR1)&(~SPI_CR1_DFF);		//8 bits format
-
-	SPI1->CR1 = ((SPI1->CR1)&(~ SPI_CR1_BR))|SPI_CR1_BR_0|SPI_CR1_BR_1;	//n = 3 -> f/16 = 84/16 = 5.25MHz
-
-	SPI1->CR1 = (SPI1->CR1)|SPI_CR1_SSM;		//nCs input mode
-	SPI1->CR1 = (SPI1->CR1)|SPI_CR1_SSI;		//Software management -> '1'
+void writeAccel(int32_t reg, int32_t data) {
 
 
-	writeAccel(0x20,0x47);	//PD = 1, X,Y,Z = 1
 
-	SPI1->CR1 = (SPI1->CR1)|SPI_CR1_SPE;		//Peripheral enabled
-	SPI1->CR1 = (SPI1->CR1)|SPI_CR1_MSTR;		//Controller is Master
+	// Although it is not mandatory for read,
+	// we verify its is not a reserved register
+	if (reg < 0x0F)
+		return;
+	if ((reg < 0x20) && (reg > 0x0F))
+		return;
+	if ((reg < 0x27) && (reg > 0x23))
+		return;
+	if ((reg < 0x30) && (reg > 0x2D))
+		return;
+
+	// return if we try to write in read - only registers
+	// outputs x, y and z
+	//if ((reg == 0x29) || (reg == 0x2B) || (reg == 0x2D))	//AQUI ESTAVA EL PUTU ERROR, L'HEM ARREGLAR A BASE DE POTRA :)
+
+		//return;
+
+	// Activates Chip Select
+	LIS_CS_PORT->BSRR.H.clear = LIS_CS_BIT;
+
+	// Posem el bit RW a 0 per indicar que es una escriptura.
+	//reg &= (~BIT(7)); //Crec que en default és 0 per tant alumillor no fa falta aquesta sentencia
+
+	// Send the command
+	SPI1->DR = reg;
+
+	// Small delay before reading the buffer
+	DELAY_US(2);
+
+	// Wait to the receiver buffer to fill
+	while (!((SPI1->SR) & BIT0))
+		;
+
+	// Empty the receiver buffer
+	// We don't need the returned information
+//	val = (SPI1->DR);		//Alumillor ens ho podem estalviar
+
+	// Write data to obtain a response		// OJO
+	SPI1->DR = data;
+
+	// Small delay before reading the buffer
+	DELAY_US(2);
+
+	// Wait to the receiver buffer to fill
+	while (!((SPI1->SR) & BIT0))
+		;
+
+	// Recover the response in the reception buffer
+	//val = (SPI1->DR);
+
+	// Deactivates Chip Select
+	LIS_CS_PORT->BSRR.H.set = LIS_CS_BIT;
+
+	return;
+}
 
 
-	DELAY_US(3000);
+void initAccel(void) {
 
+	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 
- }
+	// Configure the CR1 register
+	// Configure the CPOL and CPHA registers
+	SPI1->CR1 |= SPI_CR1_CPOL | SPI_CR1_CPHA;
 
+	// Configure the DFF
+	SPI1->CR1 &= (~SPI_CR1_DFF);
+
+	// Configure the BR [2:0]
+	SPI1->CR1 = (SPI1->CR1 | SPI_CR1_BR) & (~SPI_CR1_BR_2);
+
+	// Configure the SSM i SSI
+	SPI1->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI;
+
+	// Activate SPE i MSTR in CR1 register
+	SPI1->CR1 |= SPI_CR1_SPE | SPI_CR1_MSTR;
+
+	GPIOE->MODER |= BIT(6);
+	GPIOE->BSRR.H.set = BIT(3);
+
+	writeAccel(0x20,0x47);
+
+	/*
+	 // Habilitar el rellotge del perifèric SPI1 del bus APB2
+	 RCC->APB2ENR |= (RCC_APB2ENR_SPI1EN);
+
+	 //Habilitar el CPHA i CPOL que són el començament del flanc i el tipus de flanc.
+	 SPI1 -> CR1 |=  SPI_CR1_CPHA;
+	 SPI1 -> CR1 |= SPI_CR1_CPOL;
+
+	 //Posem el DFF en 0 per tenir-lo en baix nivell
+	 SPI1-> CR1 &= (~SPI_CR1_DFF);
+
+	 //Activem la fclock adequada trobada a EP1
+	 SPI1 ->CR1 = ((SPI1->CR1)&(~SPI_CR1_BR))|(SPI_CR1_BR_1);//0 o 1
+	 SPI1 ->CR1 = ((SPI1->CR1)&(~SPI_CR1_BR))|(SPI_CR1_BR_2);//1 o 2// "~" alt gr +4
+
+	 //Activem el SSM i SSI
+	 SPI1->CR1 |= SPI_CR1_SSM;
+	 SPI1->CR1 |= SPI_CR1_SSI;
+
+	 //De moment el pin PE3 no l'activarem
+
+	 //Ara activem el SPE i MSTR que s'han d'activar els últims.
+	 SPI1->CR1 |= SPI_CR1_MSTR;
+	 SPI1->CR1 |= SPI_CR1_SPE;
+	 */
+
+	/*
+	 0x20C7 - Write value 0xC7 into register 0x20 (CTRL_REG1)
+	 0xAB00 - Read value from register 0x2B (OUT_Y)
+
+	 0x00 (WRITE) + 0x20 (CTRL_REG1) = 0x20
+	 0x80 (READ) + 0x2B (OUT_Y) = 0xAB
+
+	 The high order byte is the Command, and Read/Write direction,
+	 the low order byte is the value to Write if sending data to a register.
+
+	 private const byte CTRL_REG1_ADDR=0x20;
+	 private const byte OUT_X_ADDR=0x29;
+	 private const byte OUT_Y_ADDR =0x2B;
+	 private const byte OUT_Z_ADDR=0x2D;
+
+	 */
+	// This code is requested in  P3.3 i P3.5
+}
 
 /********** PUBLIC FUNCTIONS ALREADY IMPLEMENTED ***************
  Those functions are provided to reduce the codding effort
  for the accelerometer module
-****************************************************************/
+ ****************************************************************/
 
 // Read an accelerometer register
 //      reg   : Number of the register to read
@@ -57,115 +160,77 @@ void initAccel(void)
 //
 // In case of error returns numbers greater than 1000
 //             1001 : Try to access a reserved register
-
-int32_t readAccel(int32_t reg,int32_t sign) {
-
-	uint8_t  val;      // Read value
+int32_t readAccel(int32_t reg, int32_t sign) {
+	int8_t val;      // Read value
 	int8_t *psigned;  // Pointer to signed value
 
-	 // Limit the register number to 6 bits 0..63
-	 reg&=0x3B;		//Depends on the accelerometer version
+	// Limit the register number to 6 bits 0..63
 
-	 // Although it is not mandatory for read,
-	 // we verify its is not a reserved register
-	 if (reg<0x0F) return 1001;
-	 if ((reg<0x20)&&(reg>0x0F)) return 1001;
-	 if ((reg<0x27)&&(reg>0x23)) return 1001;
-	 if ((reg<0x30)&&(reg>0x2D)) return 1001;
+	reg &= 0x3F;
 
-	 // Activates Chip Select
-	 LIS_CS_PORT->BSRR.H.clear=LIS_CS_BIT;
+	// Although it is not mandatory for read,
+	// we verify its is not a reserved register
+// if (reg<0x0F) return 1001;
+	if ((reg < 0x20) && (reg > 0x0F))
+		return 1001;
+	if ((reg < 0x27) && (reg > 0x23))
+		return 1001;
+	if ((reg < 0x30) && (reg > 0x2D))
+		return 1001;
 
-	 // Creates the command
-	 //  6 low bits: Register to read
-	 //      bit 6 : 0 Disabe autoincrement
-	 //      bit 7 : 1 Enable autoincrement
-	 reg|=BIT(7);
+	// Activates Chip Select
+	LIS_CS_PORT->BSRR.H.clear = LIS_CS_BIT;
 
-	 // Send the command
-	 SPI1->DR=reg;
+	// Creates the command
+	//  6 low bits: Register to read
+	//      bit 6 : 0 Disabe autoincrement
+	//      bit 7 : 1 Enable autoincrement
+	reg |= BIT(7);
 
-	 // Small delay before reading the buffer
-	 DELAY_US(2);
+	// Send the command
+	SPI1->DR = reg;
 
-	 // Wait to the receiver buffer to fill
-	 while (!((SPI1->SR) & BIT0));
+	// Small delay before reading the buffer
+	DELAY_US(2);
 
-	 // Empty the receiver buffer
-	 // We don't need the returned information
-	 val=(SPI1->DR);
+	// Wait to the receiver buffer to fill
+	while (!((SPI1->SR) & BIT0))
+		;
 
-	 // Write dummy data to obtain a response
-	 SPI1->DR=0xFF;
+	// Empty the receiver buffer
+	// We don't need the returned information
+	val = (SPI1->DR);
 
-	 // Small delay before reading the buffer
-	 DELAY_US(2);
+	// Write dummy data to obtain a response
+	SPI1->DR = 0xFF;
 
-	 // Wait to the receiver buffer to fill
-	 while (!((SPI1->SR) & BIT0));
+	// Small delay before reading the buffer
+	DELAY_US(2);
 
-	 // Recover the response in the reception buffer
-	 val=(SPI1->DR);
+	// Wait to the receiver buffer to fill
+	while (!((SPI1->SR) & BIT0))
+		;
 
-	 // Deactivates Chip Select
-	 LIS_CS_PORT->BSRR.H.set=LIS_CS_BIT;
+	// Recover the response in the reception buffer
+	val = (SPI1->DR);
 
-	 // The obtained data will be uint8 (0..255)
+	// Deactivates Chip Select
+	LIS_CS_PORT->BSRR.H.set = LIS_CS_BIT;
 
-	 // Return the read value if sign is false
-	 if (!sign) return val;
+	// The obtained data will be uint8 (0..255)
 
-	 // If the data is signed we need to return in two's complement
+	// Return the read value if sign is false
+	if (!sign)
+		return val;
 
-	 // Point to the returned value
-	 psigned=&val;
+	// If the data is signed we need to return in two's complement
 
-	 // Return as signed value
-	 return *psigned;
- }
+	// Point to the returned value
+	psigned = &val;
 
-
-int32_t writeAccel(int32_t reg,int32_t data){
-
-	 // Limit the register number to 6 bits 0..63
-	 reg&=0x3B;
-
-	 // Although it is not mandatory for read,
-	 // we verify its is not a reserved register
-	 if (reg<0x0F) return 1001;
-	 if ((reg<0x20)&&(reg>0x0F)) return 1001;
-	 if ((reg<0x27)&&(reg>0x23)) return 1001;
-	 if ((reg<0x30)&&(reg>0x2D)) return 1001;
-
-	 LIS_CS_PORT->BSRR.H.clear=LIS_CS_BIT;
-
-	 reg&=(!BIT(7));
-
-   	 SPI1->DR=reg;
-
-	 while (!((SPI1->SR) & BIT0));
-
-	  // Empty the receiver buffer
-	  // We don't need the returned information
-	  val=(SPI1->DR);
-
-	  SPI1->DR=data;		//PD = 1, X,Y,Z = 1
-
-	  // Small delay before reading the buffer
-	  DELAY_US(2);
-
-	  // Wait to the receiver buffer to fill
-	  while (!((SPI1->SR) & BIT0));
-
-	  // Recover the response in the reception buffer
-	  val=(SPI1->DR);
-
-	  // Deactivates Chip Select
-	  LIS_CS_PORT->BSRR.H.set=LIS_CS_BIT;
-
-	  return 1;
+	// Return as signed value
+	return *psigned;
 }
-
 
 
 
@@ -182,48 +247,103 @@ int32_t writeAccel(int32_t reg,int32_t data){
 //                  2  Binary
 //                  10 Decimal
 //                  16 Hexadecimal
-char *itoa(int32_t num, char *str, int32_t radix){
-	 int32_t sign = 0;   // To remember the sign if negative
-	 int32_t pos = 0;    // String position
-	 int32_t i;          // Generic use variable
+char *itoa(int32_t num, char *str, int32_t radix) {
+	int32_t sign = 0;   // To remember the sign if negative
+	int32_t pos = 0;    // String position
+	int32_t i;          // Generic use variable
 
-	 //Save sign
-	 if (num < 0){
-		   sign = 1;
-		   num = -num;
-	 }
-	 //Construct a backward string of the number.
-	 do {
+	//Save sign
+	if (num < 0) {
+		sign = 1;
+		num = -num;
+	}
+
+	//Construct a backward string of the number.
+	do {
 		i = num % radix;
-		if (i < 10){
-			 str[pos] = i + '0';
-		}else{
-			 str[pos] = i - 10 + 'A';
-		}
+		if (i < 10)
+			str[pos] = i + '0';
+		else
+			str[pos] = i - 10 + 'A';
 		pos++;
 		num /= radix;
-	 }while (num > 0);
+	} while (num > 0);
 
-	 //Now add the sign
-	 if (sign){
-		   str[pos] = '-';
-	 }else{
-		   pos--;
-	 }
+	//Now add the sign
+	if (sign)
+		str[pos] = '-';
+	else
+		pos--;
 
-	 // Add the final null
-	  str[pos+1]=0;
+	// Add the final null
+	str[pos + 1] = 0;
 
-	 // Pos is now the position of the final digit (before null)
+	// Pos is now the position of the final digit (before null)
 
-	 // Now reverse the string
-	 i=0;
-	 do{
-		  sign=str[i];
-		  str[i++]=str[pos];
-		  str[pos--]=sign;
-	  }while(i<pos);
+	// Now reverse the string
+	i = 0;
+	do {
+		sign = str[i];
+		str[i++] = str[pos];
+		str[pos--] = sign;
+	} while (i < pos);
 
-	 return str;
+	return str;
+}
+/*
+ void visualInclination(void) {
+
+ int32_t x, y, min, max, posx, posy, antx, anty, N;
+ uint32_t i;
+ N = 100;
+ // Min and max are the values expected for 45 and -45 degrees
+
+ min = -39;
+ max = 39;
+
+ while (1) {
+ x = 0;
+ y = 0;
+ // We make the N samples average of x and y to avoid noise
+
+ for (i = 0; i < N; i++) {
+ x += readAccel(0x29, 1);
+ y += readAccel(0x2B, 1);
  }
+ x = x / N;
+ y = y / N;
+
+ /* We make a data transformation to have a integer value between 0 and 15 being 0
+ the value asociated to -45 degrees and 15 the asociated to 45 degrees .
+ We always avoid going out the range [0 ,15].
+
+ posx = 15 * (x - min) / (max - min);
+ if (posx < 0)
+ posx = 0;
+ else if (posx > 15)
+ posx = 15;
+ posy = 15 * (y - min) / (max - min);
+ if (posy < 0)
+ posy = 0;
+ else if (posy > 15)
+ posy = 15;
+
+ /* We erase the (*) characters and set them
+ in their corresponding position . Then we
+ actualize the positions.
+
+ LCD_GotoXY(antx, 0);
+ LCD_SendChar(' ');
+ LCD_GotoXY(anty, 1);
+ LCD_SendChar(' ');
+ LCD_GotoXY(posx, 0);
+ LCD_SendChar('*');
+ LCD_GotoXY(posy, 1);
+ LCD_SendChar('*');
+ antx = posx;
+ anty = posy;
+ }
+ }*/
+
+
 
